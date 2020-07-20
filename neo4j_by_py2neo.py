bf5d74b0e9cc86a8541d19c4552e0234bf41e8c4
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2020-07-20 18:17:49 +0800

    通过py2neo在neo4j建立图谱关系

---
+++ neo4j_by_py2neo.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# @Author: xiaodong
+# @Date  : 2020/7/20
+
+import json
+
+from py2neo import NodeMatcher
+from py2neo import Graph, Node, Relationship
+
+g = Graph("bolt://localhost:7687", password="neo")
+matcher = NodeMatcher(g)
+
+
+if __name__ == '__main__':
+
+    def load_json(path):
+        with open(path, encoding="utf-8") as file:
+            data = json.load(file)
+        return data
+
+    records = load_json("data/dataset/movies.json")
+
+    movies = set()
+    actors = set()
+
+
+    history = {}
+    mapping = {
+        "name": "电影名称",
+        "to_url": "百度链接",
+        "actors": "演员名称",
+    }
+    for movie, detail in records.items():
+        for k, v in detail.items():
+            k = mapping.get(k, k)
+            if isinstance(v, list):
+                v = list(filter(lambda obj: obj.strip(), v))
+                history.setdefault(k, set()).update(set(v))
+            else:
+                if v.strip():
+                    history.setdefault(k, set()).add(v)
+
+    tx = g.begin()
+    for label, aset in history.items():
+        for element in aset:
+            node = Node(label, **{"label": element})
+            tx.create(node)
+    tx.commit()
+
+    tx = g.begin()
+    for movie, detail in records.items():
+        movie_nodes = matcher.match(**{"label": movie})
+        for movie_node in movie_nodes:
+            for k, v in detail.items():
+                if k == "name":
+                    continue
+                if isinstance(v, list):
+                    for _v in v:
+                        nodes = matcher.match(**{"label": _v})
+                        for node in nodes:
+                            relation = Relationship(movie_node, mapping.get(k, k), node)
+                            tx.create(relation)
+                else:
+                    nodes = matcher.match(**{"label": v})
+                    for node in nodes:
+                        relation = Relationship(movie_node, mapping.get(k, k), node)
+                        tx.create(relation)
+    tx.commit()
